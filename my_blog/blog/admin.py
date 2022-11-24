fc79c59514d027a523d146333a07c531d7750c09
Francisco Alvidrez <falvid16@gmail.com>
2022-11-24 11:46:07 -0700

    Registered the articles model in the admin site and added the prepolutated slug field. Created the article detail view and added slug url to url patterns

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+# admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Article
 
 # Create your views here.
+class ArticleList(ListView):
+    model = Article
+    context_object_name = "articles"
+    template_name = "blog/articles.html"
+    paginate_by = 5
+
+class ArticleDetails(DetailView):
+    model = Article
+    template_name = "blog/article_details.html"
