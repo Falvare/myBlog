bc48224eca600119ab6e5b960d5b249ab1875719
Francisco Alvidrez <falvid16@gmail.com>
2022-11-23 17:10:42 -0700

    Started project, changed project structure, setup env variables and postgres db

---
+++ config/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-727o8kg)*ekmq&gay8s9x66g5x&6luaetm*)foj68q5(acc)(z'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
