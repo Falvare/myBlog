ed3cbb7599d97d6f1e4f5e5feb80a5c85ee2944a
Francisco Alvidrez <falvid16@gmail.com>
2022-11-23 18:05:10 -0700

    Created article model and made migrations

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    author = models.CharField(max_length=250, default="Francisco J. Alvidrez")
+    published = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-published']
+
+    def __str__(self):
+        return self.title
