5ac0104e816e4302cc007658f51a3fa822015bb7
Francisco Alvidrez <falvid16@gmail.com>
2022-11-28 13:46:21 -0700

    Added url to allowed host

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,4 +7,4 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fja-projects.onrender.com']
