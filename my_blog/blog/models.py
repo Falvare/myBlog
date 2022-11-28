6d8099b8a92f1fc90bd3b56dcedfff7be1b9da92
Francisco Alvidrez <falvid16@gmail.com>
2022-11-28 12:48:32 -0700

    Modified the article model to include image fields and seperate sections, and made migrations

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    body = models.TextField()
+    github = models.URLField(blank=True)
+    url = models.URLField(blank=True)
+    image1 = CloudinaryField(null=True, blank=True)
+    image2 = CloudinaryField(null=True, blank=True)
+    image3 = CloudinaryField(null=True, blank=True)
+    technologies = models.TextField(blank=True)
+    sectionLabel1 = models.CharField(max_length=250, blank=True)
+    section1 = models.TextField()
+    sectionLabel2 = models.CharField(max_length=250, blank=True)
+    section2 = models.TextField(blank=True)
+    sectionLabel3 = models.CharField(max_length=250, blank=True)
+    section3 = models.TextField(blank=True)
     author = models.CharField(max_length=250, default="Francisco J. Alvidrez")
     published = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
