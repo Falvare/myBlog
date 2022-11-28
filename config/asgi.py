7a6c6eefc1ae0712f23c5c6e43f588983f0b29ee
Francisco Alvidrez <falvid16@gmail.com>
2022-11-28 13:38:58 -0700

    Fixed settings reference in asgi and wsgi file

--- config/asgi.py
+++ config/asgi.py
@@ -13,6 +13,6 @@ import dotenv
 dotenv.load_dotenv()
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
